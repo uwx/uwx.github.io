e1b18320753bd465449a260821478b2443e34b37
uwx <maxine@cybergal.com>
2025-01-12 21:51:40 -0300

    Add renderMarkdownFancy + expressive-code + use remark/rehype instead of marked

--- src/lib/render-markdown.ts
+++ src/lib/render-markdown.ts
@@ -1,6 +1,30 @@
-import { marked } from 'marked';
+import remarkHtml from 'remark-html';
+import remarkParse from 'remark-parse';
+import { createResource, createSignal, type Accessor, type JSX, type Signal } from 'solid-js';
+import { unified } from 'unified';
+import remarkRehype from 'remark-rehype';
+import rehypeExpressiveCode, { type RehypeExpressiveCodeOptions } from 'rehype-expressive-code';
+import rehypeSanitize from 'rehype-sanitize';
+import rehypeStringify from 'rehype-stringify';
 
 export async function renderMarkdown(content: string) {
-    content = await marked.parse(content);
-    return content;
+    const vfile = await unified()
+        .use(remarkParse)
+        .use(remarkHtml)
+        .process(content);
+    return String(vfile);
+}
+
+export async function renderMarkdownFancy(markdown: string) {
+    const vfile = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeSanitize)
+        .use(rehypeExpressiveCode, {
+
+        } satisfies RehypeExpressiveCodeOptions)
+        .use(rehypeStringify)
+        .process(markdown);
+
+    return String(vfile);
 }
